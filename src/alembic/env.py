4459df1c114c4fcd94a9c16a728414a5183ea06e
Nikeshhh <wityazkajit@gmail.com>
2024-08-27 08:30:57 +0300

    moved alembic into src to perform migrations

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 from src.config import settings
 from src.database.models import Base
+from src.auth.models import User  # noqa: F401
 
 
 target_metadata = Base.metadata
